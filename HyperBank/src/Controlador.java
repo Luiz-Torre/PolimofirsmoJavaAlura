f6b48f5ed3a8844a09d8b2e1ce66659820a335aa
Luiz Torre <69131416+Luiz-Torre@users.noreply.github.com>
2022-04-03 13:33:55 -0300

    Update Controlador.java

--- HyperBank/src/Controlador.java
+++ HyperBank/src/Controlador.java
@@ -1,5 +1,12 @@
 public class Controlador {
     public static void main(String[] args){
-        
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Luiz T");
+        funcionario.setCpf("123123123-12");
+        funcionario.setSalario(35967.68);
+
+        System.out.println(funcionario.getNome());
+        System.out.println(funcionario.getBonificacao());
+
     }
 }
