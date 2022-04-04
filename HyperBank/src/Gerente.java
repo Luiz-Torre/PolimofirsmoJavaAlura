6e19ba0fd11bb51640d8f748c6a932efaaf718c3
Luiz Torre <69131416+Luiz-Torre@users.noreply.github.com>
2022-04-04 17:54:54 -0300

    Atualizando visibilidade do atributo salario

--- HyperBank/src/Gerente.java
+++ HyperBank/src/Gerente.java
@@ -7,11 +7,11 @@ public class Gerente extends Funcionario {
     public boolean autentica(int senha) {
         return this.senha == senha;
     }
-    
+
     public void setSenha(int senha) {
         this.senha = senha;
     }
     public double getBonificacao() {
-        return this.salario;
+        return super.salario;
     }
 }
