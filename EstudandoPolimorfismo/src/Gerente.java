0ae68f744577e84c6bbe2b8beb9636c55af26c7a
Luiz Torre <69131416+Luiz-Torre@users.noreply.github.com>
2022-04-07 18:29:26 -0300

    Atualizando o nome

--- EstudandoPolimorfismo/src/Gerente.java
+++ EstudandoPolimorfismo/src/Gerente.java
@@ -1,6 +1,5 @@
-//Gerente eh um FuncionarioAutenticavel, Gerente herda da classe FuncionarioAutenticavel
 
-public class Gerente extends FuncionarioAutenticavel {
+public class Gerente extends Funcionario {
 
     public double getBonificacao() {
         System.out.println("Chamando o método de bonificacao do GERENTE");
