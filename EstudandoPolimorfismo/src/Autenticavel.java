7a9b80dca503d57f1d079a32549b06a8dae5be3a
Luiz Torre <69131416+Luiz-Torre@users.noreply.github.com>
2022-04-08 17:48:09 -0300

    Finalizando aula sobre polimorfismo

--- EstudandoPolimorfismo/src/Autenticavel.java
+++ EstudandoPolimorfismo/src/Autenticavel.java
@@ -1,7 +1,7 @@
 public abstract interface Autenticavel {
 
-    public abstract void setSenha(int senha);
+    void setSenha(int senha);
 
-    public abstract boolean autentica(int senha);
+    boolean autentica(int senha);
 
 }
